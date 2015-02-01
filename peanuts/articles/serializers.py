daca1038885df502a3fdbaae3a0b110e5f9db63e
ssbrewster <ssbrewster@gmail.com>
2015-02-01 19:53:38 +0000

    Article Serializer
    HyperLinkedModelSerializer for the Article model
    with a HyperlinkedRelatedField to author

--- peanuts/articles/serializers.py
+++ peanuts/articles/serializers.py
@@ -1,1 +1,11 @@
+from django.forms import widgets
+from rest_framework import serializers
+from .models import Article
 
+
+class ArticleSerializer(HyperLinkedModelSerializer):
+    author = serializers.HyperlinkedRelatedField(view_name='author-detail', read-only='True')
+
+    class Meta:
+        model = Article
+        fields = ('url', 'author', 'title', 'body')
