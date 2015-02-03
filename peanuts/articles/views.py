7660b9dfa2961c4adace8b43b91894e590d0a588
ssbrewster <ssbrewster@gmail.com>
2015-02-03 16:07:05 +0000

    ArticleViewSet
    This viewset automatically provides `list`, `create`, `retrieve`,
    `update` and `destroy` actions.

--- peanuts/articles/views.py
+++ peanuts/articles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Article
+from .serializers import ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
