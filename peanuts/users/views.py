2e6071e0a193b4c7d7afab719cd13afdec28ca90
ssbrewster <ssbrewster@gmail.com>
2015-02-12 22:12:22 +0000

    User Viewset
    User viewset automatically provides `list`, `create`,
    `retrieve`, `update` and `destroy` actions.

--- peanuts/users/views.py
+++ peanuts/users/views.py
@@ -1,29 +1,20 @@
 # -*- coding: utf-8 -*-
-# Import the reverse lookup function
 from django.core.urlresolvers import reverse
-
-# view imports
-from django.views.generic import DetailView
-from django.views.generic import RedirectView
-from django.views.generic import UpdateView
-from django.views.generic import ListView
-
-# Only authenticated users can access views using this.
+from rest_framework import viewsets
 from braces.views import LoginRequiredMixin
-
-# Import the form from users/forms.py
-from .forms import UserForm
 
 # Import the customized User model
 from .models import User
+from .serializers import UserSerializer
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = "username"
-    slug_url_kwarg = "username"
-
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
@@ -33,25 +24,3 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
                        kwargs={"username": self.request.user.username})
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    form_class = UserForm
-
-    # we already imported User in the view code above, remember?
-    model = User
-
-    # send the user back to their own page after a successful update
-    def get_success_url(self):
-        return reverse("users:detail",
-                       kwargs={"username": self.request.user.username})
-
-    def get_object(self):
-        # Only get the User record for the user making the request
-        return User.objects.get(username=self.request.user.username)
-
-
-class UserListView(LoginRequiredMixin, ListView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = "username"
-    slug_url_kwarg = "username"
