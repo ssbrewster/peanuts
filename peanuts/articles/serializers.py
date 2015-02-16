fb6ba036e43b801cee6fd9756cce8aead84b6e49
ssbrewster <ssbrewster@gmail.com>
2015-02-16 21:28:22 +0000

    Author related field read_only set to True

--- peanuts/articles/serializers.py
+++ peanuts/articles/serializers.py
@@ -3,7 +3,7 @@ from .models import Article
 
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.HyperlinkedRelatedField(view_name='user-detail', lookup_field='username')
+    author = serializers.HyperlinkedRelatedField(view_name='user-detail', lookup_field='username', read_only='True')
 
     class Meta:
         model = Article
